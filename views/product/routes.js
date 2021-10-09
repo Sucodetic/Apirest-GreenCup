91de22da068d10843f1fbb0e22c44cc614f9f00b
Jhonathan Pérez Ortega <jhperez.ortega@gmail.com>
2021-10-09 15:27:24 -0500

    Agrega rutas para productos

---
+++ views/product/routes.js
@@ -0,0 +1,30 @@
+import Express from "express";
+import { createProduct, deleteProduct, editProduct, getAllProducts } from "../../controllers/product/productController.js";
+
+const rutasProducto = Express.Router();
+
+const genericCallback = (res) => (err, result) => {
+  if (err) {
+    res.status(500).send(err);
+  } else {
+    res.json(result);
+  }
+};
+
+rutasProducto.route("/productos").get((req, res) => {
+  getAllProducts(genericCallback(res));
+});
+
+rutasProducto.route("/productos").post((req, res) => {
+  createProduct(req.body, genericCallback(res));
+});
+
+rutasProducto.route("/productos/:id").patch((req, res) => {
+  editProduct(req.params.id, req.body, genericCallback(res));
+});
+
+rutasProducto.route("/productos/:id").delete((req, res) => {
+  deleteProduct(req.params.id, genericCallback(res));
+});
+
+export default rutasProducto;
