0734c1891e9d59bc4c15e9375b1a8ba9ebc1414e
Jhonathan Pérez Ortega <jhperez.ortega@gmail.com>
2021-10-15 12:11:47 -0500

    Implementa Api Auth0 para configuración tokens

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ import { connectServer } from "./db/db.js";
 import rutasProducto from "./views/product/routes.js";
 import rutasUsuario from "./views/user/routes.js";
 import rutasVenta from "./views/sale/routes.js";
+import jwt from "express-jwt";
+import jwks from "jwks-rsa";
 
 dotenv.config({ path: "./.env" });
 
@@ -16,6 +18,21 @@ app.set("json spaces", 2);
 app.use(morgan("dev"));
 app.use(Express.json());
 app.use(Cors());
+
+var jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: "https://greencup.us.auth0.com/.well-known/jwks.json",
+  }),
+  audience: "api-autenticacion-greencup",
+  issuer: "https://greencup.us.auth0.com/",
+  algorithms: ["RS256"],
+});
+
+app.use(jwtCheck);
+
 app.use(rutasProducto);
 app.use(rutasUsuario);
 app.use(rutasVenta);
